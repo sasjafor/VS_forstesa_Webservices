d5bf3a17e38784aa0f1ce20eb679eb30d9a6e51a
Sascha <sascha.forster@hotmail.ch>
2017-10-13 09:56:29 +0200

    basic setup

---
+++ app/src/main/java/ch/ethz/inf/vs/a2/webservices/RestClient.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.a2.webservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest_client);
+    }
+}
---
+++ app/src/main/java/ch/ethz/inf/vs/a2/webservices/RestServer.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.a2.webservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestServer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest_server);
+    }
+}
